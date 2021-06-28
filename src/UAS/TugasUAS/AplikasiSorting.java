e9b64b0adcd87e119189efaf3bc3685640855eaa
Nurul Nabilah Sitorus <12050121664@students.uin-suska.ac.id>
2021-06-28 19:31:08 +0700

    Pemrograman Lanjut

--- src/UAS/TugasUAS/AplikasiSorting.java
+++ src/UAS/TugasUAS/AplikasiSorting.java
@@ -6,72 +6,74 @@ import java.awt.event.ActionListener;
 import java.util.Arrays;
 
 public class AplikasiSorting {
-    private JTextField textField;
-    private JButton OKButton;
-    private JButton acButton;
-    private JButton exitButton;
-    private JPanel nolPanel;
-    private JLabel hasilJLabel;
+    private JTextField textField;   // sebuah konstruktor JTextField dari form aplikasi java GUI yang hanya bisa diakses di dalam class AplikasiSorting
+    private JButton OKButton;   // sebuah konstruktor JButton dari form aplikasi java GUI yang hanya bisa diakses di dalam class AplikasiSorting
+    private JButton deleteButton;   // sebuah konstruktor JButton dari form aplikasi java GUI hanya bisa diakses di dalam class AplikasiSorting
+    private JButton exitButton; // sebuah konstruktor JButton dari form aplikasi java GUI yang hanya bisa diakses di dalam class AplikasiSorting
+    private JPanel nolPanel;    // sebuah konstruktor JPanel dari form aplikasi java GUI yang hanya bisa diakses di dalam class AplikasiSorting
+    private JLabel hasilJLabel; // sebuah konstruktor JLabel dari form aplikasi java GUI yang hanya bisa diakses di dalam class AplikasiSorting
 
 
-    public AplikasiSorting() {
-        OKButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
+    public AplikasiSorting() {  // sebuah method khusus yang berisi objek-objek yang akan dieksekusi
+        OKButton.addActionListener(new ActionListener() {   // sebuah interaksi yang dilakukan pada tombol di aplikasi untuk menjalankan proses dan menampilkan hasilnya setelah menekan tombolnya
+            @Override   // kemampuan sebuah kode program untuk membuat subclass di dalam class AplikasiSorting
+            public void actionPerformed(ActionEvent e) {    // sebuah method yang dapat diakses oleh objek lain, tetapi tidak dapat mengembalikan nilai
 
-                String angka =  textField.getText();
-                // ubah ke array of strings
-                String[] arrStr = angka.split(" ", 0);
-                // declare array of integers
-                int[] arrInt = new int[arrStr.length];
-                // ubah masing-masing data array menjadi integer
-                for (int m = 0; m < arrInt.length; m++)
+                String angka =  textField.getText();    // proses mendeklarasi dan menginisialisasi variabel sekaligus dan mengambil data dari textfield pada form java GUI
+
+                String[] arrStr = angka.split(" ", 0);  // menukarkan variabel angka menjadi variabel arrStr
+
+                int[] arrInt = new int[arrStr.length];  // membuat array yang baru
+
+                for (int m = 0; m < arrInt.length; m++) // memeriksa kondisi, lalu mengeksekusi variabel arrInt
                 {
-                    arrInt[m] = Integer.parseInt(arrStr[m]);
+                    arrInt[m] = Integer.parseInt(arrStr[m]);    // mengkonversikan variabel arrInt ke dalam bentuk data Int
                 }
 
-                // algoritme sort
-                int n = arrInt.length;
+                // Algoritme Sort dengan menggunakan Bubble Sort
+                int n = arrInt.length;  // mendeklarasikan dan menukarkan arrInt.length tadi ke variabel n untuk dieksekusi di algoritma sort
 
-                for (int i = 0; i < n-1; i++)
-                {
-                    for (int j = 0; j < n-i-1; j++) {
-                        if (arrInt[j] > arrInt[j+1])
-                        {
+                for (int i = 0; i < n-1; i++) {     // memeriksa kondisi terlebih dahulu, lalu mengeksekusi program untuk mengulangi semua kodingan sebanyak jumlah data yang diinput
+                    for (int j = 0; j < n-i-1; j++) {   // memeriksa kondisi terlebih dahulu, lalu mengeksekusi program yang akan diurutkan datanya dalam bentuk int
+                        if (arrInt[j] > arrInt[j+1]) {  // program akan membandingkan data ke-j dengan data ke-(j+1) guna mengurutkan data secara ascending
+
                             // swap arr[j+1] and arr[j]
-                            int temp = arrInt[j];
-                            arrInt[j] = arrInt[j+1];
-                            arrInt[j+1] = temp;
+                            int baru = arrInt[j]; // menukarkan variabel j tadi ke variabel baru
+                            arrInt[j] = arrInt[j+1];    // menukarkan variabel (j+1) ke variabel j
+                            arrInt[j+1] = baru; // menukarkan variabel yang baru ke variabel (j+1)
                         }
                     }
                 }
 
                 // convert array to string
-                String string = Arrays.toString(arrInt);
+                String string = Arrays.toString(arrInt);    // hasil yang telah dieksekusi tadi dikonversi ke bentuk data string
                 // Hasil
-                hasilJLabel.setText(string);
+                hasilJLabel.setText(string);    // hasil output pada program ini akan tampil di form java GUI
             }
         });
 
-        acButton.addActionListener(new ActionListener() {
+        deleteButton.addActionListener(new ActionListener() {   // sebuah interaksi yang dilakukan pada tombol di aplikasi untuk membersihkan atau menghapus hasil output dan juga me-reset data terakhir yang telah diinput setelah menekan tombolnya
             @Override
-            public void actionPerformed(ActionEvent e) {
-                textField.setText("");
-                hasilJLabel.setText("");
-                textField.requestFocus();
+            public void actionPerformed(ActionEvent e) {    // sebuah method yang dapat diakses oleh objek lain, tetapi tidak dapat mengembalikan nilai
+                textField.setText("");  // program akan menjalankan perintah untuk mengosongkan data yang telah diinput
+                hasilJLabel.setText("");    // program akan menjalankan perintah untuk mengosongkan hasil output
+                textField.requestFocus();   // jika sudah diproses, maka kode tersebut akan mengarahkan kursor ke posisi textField
             }
         });
 
-        exitButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.exit(0);
+        exitButton.addActionListener(new ActionListener() { // sebuah interaksi yang dilakukan pada tombol di aplikasi untuk akan keluar dari aplikasi tersebut jika menekan tombolnya
+            @Override   // kemampuan sebuah kode program untuk membuat subclass di dalam class AplikasiSorting
+            public void actionPerformed(ActionEvent e) {    // sebuah method yang dapat diakses oleh objek lain, tetapi tidak dapat mengembalikan nilai
+
+                System.exit(0); // sebuah parameter exit, jika dijalankan program eksekusi berjalan baik
+
             }
         });
     }
 
-    public JPanel getNolPanel() {
-        return nolPanel;
+    public JPanel getNolPanel() {   // sebuah method khusus yang berisi objek-objek yang akan dieksekusi
+
+        return nolPanel;    // mengembalikan nolPanel ke objek lain
     }
 
 }
--- src/UAS/TugasUAS/RunAplikasiSorting.java
+++ src/UAS/TugasUAS/RunAplikasiSorting.java
@@ -3,8 +3,9 @@ package UAS.TugasUAS;
 import javax.swing.*;
 
 
-public class RunAplikasiSorting {
-    public static void main(String[] args) {
+public class RunAplikasiSorting {   // kelas atau method yang dapat diakses dari kelas manapun
+    public static void main(String[] args) {    // perintah untuk menjalankan program java
+
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         } catch (ClassNotFoundException | InstantiationException | IllegalAccessException |
@@ -14,15 +15,17 @@ public class RunAplikasiSorting {
         /**
          * set image icon
          */
-        ImageIcon imageIcon = new ImageIcon("Image/Gambar.png");
+        ImageIcon imageIcon = new ImageIcon("Image/Gambar.png");    // membuat image icon baru dari file pada form JFrame
 
-        JFrame jframe = new JFrame("Aplikasi Sortir Angka");
-        jframe.setIconImage(imageIcon.getImage());
-        jframe.setBounds(100, 100, 600, 400);
-        jframe.setContentPane(new AplikasiSorting().getNolPanel());
-        jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        jframe.setLocationRelativeTo(null);
-        jframe.setVisible(true);
+        JFrame jframe = new JFrame("Aplikasi Sortir Angka");    // membuat form JFrame dengan judul "Aplikasi Sortir Angka" di aplikasinya
+        jframe.setIconImage(imageIcon.getImage());  // membuat ikon pada form JFrame dari gambar yang dimuat dari file
+        jframe.setLayout(null); // menentukan jenis layout apa yang akan ditampilkan
+        jframe.setBounds(100, 100, 600, 400);   // mengatur posisi form JFrame dengan perintah setBounds() karena layout yang digunakan adalah null layout
+        jframe.setContentPane(new AplikasiSorting().getNolPanel()); // tempat menyimpan semua text, tombol, atau gambar yang diinginkan untuk ditampilkan
+        jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  // menjalankan form JFrame untuk menutup keseluruhan aplikasi apabila kita menutup aplikasinya
+        jframe.setLocationRelativeTo(null); // membuat form JFrame berada di tengah layar
+        jframe.setSize(600, 400);   // membuat ukuran form JFrame yang diinginkan untuk ditampilkan dilayar
+        jframe.setVisible(true);    // mengatur output agar dapat ditampilkan
 
     }
 }
